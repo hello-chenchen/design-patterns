2af40925cdbbcbbaa7221021bac02c46b5c7f02d
hello_chenchen <welcomechenchen@gmail.com>
2019-04-08 21:33:37 +0800

    optimize: add Override annotation

--- src/main/java/com/cc/designpatterns/factorypattern/FactoryMethodA.java
+++ src/main/java/com/cc/designpatterns/factorypattern/FactoryMethodA.java
@@ -1,6 +1,8 @@
 package com.cc.designpatterns.factorypattern;
 
 public class FactoryMethodA implements FactoryMethodInterface {
+
+    @Override
     public String invokeFactoryMethod() {
         System.out.println("FactoryMethodA::invokeFactoryMethod");
         return "FactoryMethodA::invokeFactoryMethod";
--- src/main/java/com/cc/designpatterns/factorypattern/FactoryMethodB.java
+++ src/main/java/com/cc/designpatterns/factorypattern/FactoryMethodB.java
@@ -1,6 +1,8 @@
 package com.cc.designpatterns.factorypattern;
 
 public class FactoryMethodB implements FactoryMethodInterface {
+
+    @Override
     public String invokeFactoryMethod() {
         System.out.println("FactoryMethodB::invokeFactoryMethod");
         return "FactoryMethodB::invokeFactoryMethod";
